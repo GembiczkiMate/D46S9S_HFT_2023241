11b834d3b0e03c194069c9e75d598ecef8264232
gembi <gembi@DESKTOP-LEPBV4A>
2023-10-18 10:25:02 +0200

    Add project files.

---
+++ D46S9S_HFT_2023241.Models/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace D46S9S_HFT_2023241.Models
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ D46S9S_HFT_2023241.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace D46S9S_HFT_2023241.Repository
+{
+    public class Class1
+    {
+    }
+}
